a98e47ab36b06abea7416bce3cd61d5dbfe1f7e2
Steven Pelletier <steve.d.pelletier@gmail.com>
2025-02-07 10:08:45 -0500

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Initialize Prism.js
+document.addEventListener("DOMContentLoaded", function () {
+  // If any code was added dynamically, we need to call Prism.highlightAll()
+  Prism.highlightAll();
+});
