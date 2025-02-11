3b59275c1a621da1587cec7875b72f6823c3697c
Steven Pelletier <steve.d.pelletier@gmail.com>
2025-02-11 03:28:08 -0500

    Add Bootstrap scrollspy functionality and enhance sidebar styles

--- scripts.js
+++ scripts.js
@@ -1,4 +1,34 @@
 // Initialize Prism.js
+// Initialize Bootstrap scrollspy
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all links in the offcanvas menu
+  const offcanvasLinks = document.querySelectorAll(".offcanvas .nav-link");
+
+  // Add click handler to each link
+  offcanvasLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      // Get the target section
+      const targetId = this.getAttribute("href");
+      const targetSection = document.querySelector(targetId);
+
+      if (targetSection) {
+        e.preventDefault();
+
+        // Close the offcanvas menu
+        const offcanvasElement = document.querySelector("#sidebar");
+        const offcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement);
+        if (offcanvas) {
+          offcanvas.hide();
+        }
+
+        // Scroll to the section after a brief delay to allow the menu to close
+        setTimeout(() => {
+          targetSection.scrollIntoView();
+        }, 350);
+      }
+    });
+  });
+});
 document.addEventListener("DOMContentLoaded", function () {
   // If any code was added dynamically, we need to call Prism.highlightAll()
   Prism.highlightAll();
