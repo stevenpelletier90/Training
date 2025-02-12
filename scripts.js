93c1d3278313886ce056c847b4ec1f4b1eb6cad6
Steven Pelletier <steve.d.pelletier@gmail.com>
2025-02-12 09:17:05 -0500

    Mobile Changes

--- scripts.js
+++ scripts.js
@@ -38,9 +38,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Add copy buttons to code blocks
   document.querySelectorAll('pre[class*="language-"]').forEach((pre) => {
+    // Create wrapper div
+    const wrapper = document.createElement("div");
+    wrapper.className = "code-block-wrapper";
+
+    // Insert wrapper before pre
+    pre.parentNode.insertBefore(wrapper, pre);
+
+    // Move pre into wrapper
+    wrapper.appendChild(pre);
+
+    // Create and add button before pre
     const button = document.createElement("button");
     button.className = "copy-button";
-    button.innerHTML = '<i class="fas fa-copy"></i> Copy';
+    button.innerHTML = '<i class="fas fa-copy"></i> Copy Code';
+    wrapper.insertBefore(button, pre);
 
     button.addEventListener("click", async () => {
       const code = pre.querySelector("code").innerText;
@@ -49,19 +61,17 @@ document.addEventListener("DOMContentLoaded", function () {
         button.innerHTML = '<i class="fas fa-check"></i> Copied!';
         button.classList.add("copied");
         setTimeout(() => {
-          button.innerHTML = '<i class="fas fa-copy"></i> Copy';
+          button.innerHTML = '<i class="fas fa-copy"></i> Copy Code';
           button.classList.remove("copied");
         }, 2000);
       } catch (err) {
         console.error("Failed to copy text: ", err);
         button.innerHTML = '<i class="fas fa-times"></i> Error';
         setTimeout(() => {
-          button.innerHTML = '<i class="fas fa-copy"></i> Copy';
+          button.innerHTML = '<i class="fas fa-copy"></i> Copy Code';
         }, 2000);
       }
     });
-
-    pre.appendChild(button);
   });
 
   // Initialize Prism.js
